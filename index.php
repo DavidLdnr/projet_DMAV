f70e2128c38488831c57bdc2595518c79c7a0c27
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-02-07 15:33:24 +0100

    création de la page d'acceuille sans le css pour le moment

---
+++ index.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="style.css">
+		<title>index</title>
+	</head>
+	<body>
+		<header>
+		<div id='btnconnexion'>
+		<?php
+		if ($_SESSION['user_id']->exist_id()==false)
+		{?>
+			<a href="lien vers la page de connexion'"><input type="button" name="connexion "value="connexion"/></a>
+		<?php}
+		else {?>
+		<a href="lien vers le script d'upload'"><input type="button" name="upload "value="upload"/></a>	
+		<a href="lien vers le script de déconnexion'"><input type="button" name="dénnexion "value="dénnexion"/></a>	
+		<?php}?>
+		</div>
+		<div id='btnconnexion'>
+		logo/nom du site
+		</div>
+		</header>
+		<div id='video'>
+		<strong>Vidéo</strong>
+		<video src="lien de la dernière vidéo sur le site" controls  width="600"></video>
+		<span id='auteur_video'>nom de l'auteur</span>
+		<span id='titre_video'>titre de la vidéo</span>
+		<span id='date_video'>date</span>
+		</div>
+		<div id='image'>
+		<strong>image</strong>
+		<img src="lien de l'image">
+		<span id='auteur_image'>nom de l'auteur</span>
+		<span id='titre_image'>titre de l'image</span>
+		<span id='date_image'>date</span>
+		</div>
+		<div id='son'>
+		<strong>Son</strong>
+		<audio src="lien du son" controls></audio>
+		<span id='auteur_son'>nom de l'auteur</span>
+		<span id='titre_son'>titre du son</span>
+		<span id='date_son'>date</span>
+		</div>
+		<footer>
+			<p>© Copyright 2017 - Marie - Antoine - David - Vincent</p>
+		</footer>
+	</body>
+</html>
