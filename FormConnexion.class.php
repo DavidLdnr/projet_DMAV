cb3195c8a804ccc1161185ab32c94728ed835c2b
VincentTescari <vincent.tescari.pro@gmail.com>
2017-02-07 16:40:18 +0100

    formconnexion

---
+++ FormConnexion.class.php
@@ -0,0 +1,11 @@
+<?php 
+class FormConnexion {
+	function __toString(){
+	return ("<form action='authentification.php'><label name='pseudo'>Pseudo</label><input type='text' value='pseudo'><br><label name='mdp'>Mot de passe</label><input type='text' value='mdp'><input type='submit' value='Valider'></form>");
+	
+	}
+	
+	
+	
+	
+	}
