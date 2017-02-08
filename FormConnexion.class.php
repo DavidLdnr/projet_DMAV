a64a1335e32e2670d0bb94914b80246fdddae1fd
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-02-08 10:37:32 +0100

    rajout des name dans les input

--- FormConnexion.class.php
+++ FormConnexion.class.php
@@ -1,7 +1,7 @@
 <?php 
 class FormConnexion {
 	function __toString(){
-	return ("<form action='index.php?page=connexion' method='post'><label name='pseudo'>Pseudo</label><input type='text' value='pseudo' required><br><label name='mdp'>Mot de passe</label><input type='text' value='mdp' required><input type='submit' value='Valider'></form>");
+	return ("<form action='index.php?page=connexion' method='post'><label name='pseudo'>Pseudo</label><input type='text' name='pseudo' value='pseudo'  autofocus required><br><label name='mdp'>Mot de passe</label><input type='text' name='mdp' value='mdp' required><input type='submit' value='Valider'></form>");
 	
 	}
 	
