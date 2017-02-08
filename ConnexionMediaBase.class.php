6453c2a83af9f622b08dfc2efee17463f3bda259
lou731 <marie.marnet.pro@gmail.com>
2017-02-08 11:10:11 +0100

    dd

--- ConnexionMediaBase.class.php
+++ ConnexionMediaBase.class.php
@@ -11,21 +11,24 @@ private $dbh;
 private function __construct() {
  $this->dbh = new PDO('mysql:host='.$this->host.';dbname='.$this->db, $this->user, $this->pass);
 }
- 
+
 
   public static function getInstance() {
     if (!self::$instance) self::$instance = new self();
     return self::$instance;
   }
-  
+
   public function get_user($pseudo){
-  	$req = "SELECT * FROM users where pseudo=$pseudo";
-	$result=$this->dbh->query($req);
-	if ($result->rowcount==1)
-		{
-		$obj=$result->fetchObject();
-		return new User($obj->id,$obj->pseudo,$obj->mdp);
-		}
-	else return null;
+  	$req = "SELECT * FROM users where pseudo='$pseudo'";
+	try
+    {
+ 	    $result=$this->dbh->query($req);
+ 		$obj=$result->fetchObject();
+ 		return new User($obj->id,$obj->pseudo,$obj->mdp);
+     }
+     catch (PDOException $exception)
+     {
+        return null;
+     }
   }
 }
