d77932d2ca7aeffa8cf4f4cd5291d7644f978870
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-02-08 12:56:17 +0100

    v2

--- accueil.php
+++ accueil.php
@@ -1,20 +1,20 @@
 <div id='video'>
     <strong>Vidéo</strong>
-    <video src="lien de la dernière vidéo sur le site" controls width="600"></video>
+    <video src="" controls width="600"></video>
     <span id='auteur_video'>nom de l'auteur</span>
     <span id='titre_video'>titre de la vidéo</span>
     <span id='date_video'>date</span>
 </div>
 <div id='image'>
     <strong>image</strong>
-    <img src="lien de l'image">
+    <img src="">
     <span id='auteur_image'>nom de l'auteur</span>
     <span id='titre_image'>titre de l'image</span>
     <span id='date_image'>date</span>
 </div>
 <div id='son'>
     <strong>Son</strong>
-    <audio src="lien du son" controls></audio>
+    <audio src="" controls></audio>
     <span id='auteur_son'>nom de l'auteur</span>
     <span id='titre_son'>titre du son</span>
     <span id='date_son'>date</span>
