3413f0796f737b48c8158a892bf1b7e63dad5cbf
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-02-09 17:13:29 +0100

    commentaire de authantification

--- Authentification.class.php
+++ Authentification.class.php
@@ -2,19 +2,21 @@
 class Authentification {
   private static $instance;
 
-
+//constructeur de la class
   private function __construct(){}
 
+  //accesseur de la l'instance
   public static function getInstance() {
       if (self::$instance == null)
           self::$instance = new Authentification();
       return self::$instance;
   }
-
+ //fonction qui appelle la fonction exist_id() pour verifier si on est authantifié ou non
   public function isAuth(){
 	return Session::getInstance()->exist_id();
 	}
 
+	//on verifie les utilisateur 
   public function checkUser($user_login, $mdp){
 	$user=ConnexionMediaBase::getInstance()->get_user($user_login);
 	if ($user != null)
@@ -34,6 +36,7 @@ class Authentification {
 	else return false;
 	}
 
+	// on déconnecte l'utilisateur en utilisant la fonction stop()
   public function disconnect(){
 	Session::getInstance()->stop();
 	}
