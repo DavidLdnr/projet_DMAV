5ba5058a06660df5b231b2d5399f86fb7d6a380c
lou731 <marie.marnet.pro@gmail.com>
2017-02-09 15:17:01 +0100

    indent code

--- FormRecherche.class.php
+++ FormRecherche.class.php
@@ -1,6 +1,12 @@
 <?php 
 class FormRecherche {
 function __toString(){
-	       return ("<form action='index.php?page=recherche' method='post'><label name='auteur'>Auteur</label><input type='text' name='auteur' value='' autofocus><br>Vidéo<input type='checkbox' name='type' value='video'>Image<input type='checkbox' name='type' value='image'>Audio<input type='checkbox' name='type' value='audio'><br><label name='description'>Description</label><input type='text' name='description' value=''><input type='submit' value='Valider'></form>");
+	       return ("<form action='index.php?page=recherche' method='post'>
+                        <label name='auteur'>Auteur</label><input type='text' name='auteur' value='' autofocus><br>
+                        Vidéo<input type='checkbox' name='type' value='video'>
+                        Image<input type='checkbox' name='type' value='image'>Audio
+                        <input type='checkbox' name='type' value='audio'><br>
+                        <label name='description'>Description</label><input type='text' name='description' value=''>
+                        <input type='submit' value='Valider'></form>");
 	}
 	}
