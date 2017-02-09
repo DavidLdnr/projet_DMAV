951e02ec3a978e65a7a9cf236cd1c48deca03e3d
lou731 <marie.marnet.pro@gmail.com>
2017-02-09 15:28:28 +0100

    first commit

--- FormRecherche.class.php
+++ FormRecherche.class.php
@@ -3,9 +3,9 @@ class FormRecherche {
 function __toString(){
 	       return ("<form action='index.php?page=recherche' method='post'>
                         <label name='auteur'>Auteur</label><input type='text' name='auteur' value='' autofocus><br>
-                        Vidéo<input type='checkbox' name='type' value='video'>
-                        Image<input type='checkbox' name='type' value='image'>Audio
-                        <input type='checkbox' name='type' value='audio'><br>
+                        Vidéo<input type='checkbox' name='video' value='video'>
+                        Image<input type='checkbox' name='image' value='image'>
+                        Audio<input type='checkbox' name='audio' value='audio'><br>
                         <label name='description'>Description</label><input type='text' name='description' value=''>
                         <input type='submit' value='Valider'></form>");
 	}
--- recherche.php
+++ recherche.php
@@ -1,14 +1,30 @@
 <?php
-		$obj = new FormRecherche;
-		if (!isset($_POST['auteur']) || !isset($_POST['description']))
-            {
-            echo $obj;
-            }
-		else if ((($_POST['auteur']!="") || ($_POST['description']!="")))
-            {
-            echo $_POST['auteur'],$_POST['description'];
-            //$recherche=ConnexionMediaBase::getInstance()->recherche($_POST['auteur'],$_POST['description'],$_POST['type']);
-            }
-        else echo $obj;
+	$obj = new FormRecherche;
+	if (!isset($_POST['auteur']) || !isset($_POST['description']))
+    {
+        echo $obj;
+    }
+	else if ($_POST['auteur']!="" || $_POST['description']!= "" ||
+        isset($_POST['audio']) || !isset($_POST['video'] || !isset($_POST['image']))
+    {
+        $types = array();
+        if(isset[isset($_POST['video']]))
+        {
+            array_push(1);
+        }
+
+        if(isset[isset($_POST['audio']]))
+        {
+            array_push(2);
+        }
+
+        if(isset[isset($_POST['image']]))
+        {
+            array_push(3);
+        }
+
+        $recherche=ConnexionMediaBase::getInstance()->recherche($_POST['auteur'], $_POST['description'], $types);
+    }
+    else
+        echo $obj;
 				
-		?>
