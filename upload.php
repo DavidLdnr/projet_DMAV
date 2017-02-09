dcdfab049d11506ec4ed9d1b7e35567492d73063
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-02-09 17:17:34 +0100

    commentaire d'upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,5 @@
 <?php
+//appelle du formulaire d'upload si le formulaire n'est pas déjà rempli sinon envoie des données vers la fonction insert_data
 		$obj = new FormUpload;
 		if (!isset($_POST['description']) and !isset($_FILES['file']) and !isset($_POST['type']))
 		{
