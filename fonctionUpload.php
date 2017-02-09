8baf3519907e5f409004db7b881f92c1d45ada36
DavidLdnr <davidalexandre.fournier.pro@gmail.com>
2017-02-09 17:38:46 +0100

    premiere mise en place de style

---
+++ fonctionUpload.php
@@ -0,0 +1,106 @@
+﻿<?php
+$date = date('Y-m-d');
+$repertoire = "./multimedia";
+$repertoire = "./multimedia/videos";
+$repertoire = "./multimedia/audio";
+$repertoire = "./multimedia/images";
+$type = $_POST['type'];
+$rep="";  
+//connection au serveur:
+  $connexion = mysqli_connect( "localhost", "root", "", "base_de_fichier") ;
+ 
+		
+if (!file_exists($repertoire))
+        
+	{
+                
+		mkdir ($repertoire,0700);
+                
+		echo " -=> Création du repertoire $repertoire réussi<br>";
+        
+	}
+	if (!file_exists($repertoire))
+        
+	{
+                
+		mkdir ($repertoire2,0700);
+                
+		echo " -=> Création du repertoire $repertoire2 réussi<br>";
+        
+	}
+	if (!file_exists($repertoire))
+        
+	{
+                
+		mkdir ($repertoire3,0700);
+                
+		echo " -=> Création du repertoire $repertoire3 réussi<br>";
+        
+	}
+	if (!file_exists($repertoire))
+        
+	{
+                
+		mkdir ($repertoire4,0700);
+                
+		echo " -=> Création du repertoire $repertoire4 réussi<br>";
+        
+	}
+if(isset($_FILES['fichier'])) // si formulaire soumis
+{ 
+    if($type == 1){
+		$content_dir = $repertoire2;// dossier où sera déplacé le fichier
+	} elseif($type == 2){
+		$content_dir = $repertoire3;// dossier où sera déplacé le fichier
+	} elseif($type == 3){
+		$content_dir = $repertoire4;// dossier où sera déplacé le fichier
+	}  
+  
+    $tmp_file = $_FILES['fichier']['tmp_name'];
+	if ($service == "0")
+	{
+		exit("IL FAUT OBLIGATOIREMEMENT CHOISIR UN NOM DE SERVICE POUR AJOUTER UN FICHIER!!!");
+	}
+    if( !is_uploaded_file($tmp_file) )
+    {
+        exit("Le fichier est introuvable");
+    }
+     
+    // on vérifie maintenant l'extension
+    $filename = $_FILES['fichier']['name'];
+    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+	if ($type == 1){
+    if($ext != 'webm')
+    {
+        exit("Le fichier n'est pas un webm,veuillez le convertir en webm s'il vous plais!!!!!!!!!!");
+    }
+	}elseif ($type == 2){
+    if($ext != 'ogg')
+    {
+        exit("Le fichier n'est pas un ogg,veuillez le convertir en ogg s'il vous plais!!!!!!!!!!");
+    }
+	}elseif ($type == 3){
+    if($ext != 'gif'||$ext != 'svg'||$ext != 'png' ||$ext != 'jpg'||$ext != 'jpeg' )
+    {
+        exit("Veuillez convertir l'image en PNG,JPG/JPEG ou en GIF s'il vous plais!!!!!!!!!!");
+    }
+	}
+    // on copie le fichier dans le dossier de destination
+    $name_file = $_FILES['fichier']['name'];
+    if(preg_match('#[\x00-\x1F\x7F-\x9F/\\\\]#', $name_file) )
+    {
+    exit("Nom de fichier non valide");
+    }
+    else if(!move_uploaded_file($tmp_file, $content_dir . $name_file))
+    {
+    exit("Impossible de copier le fichier dans $content_dir");
+    } 
+    echo "Le fichier a bien été uploadé</br>";
+
+	$chemin = ($content_dir . $name_file);
+	$nom = $name_file;
+}
+				}
+			}				
+				
+?>
--- index.php
+++ index.php
@@ -14,47 +14,45 @@ Session::getInstance()->start(); // Démarrage de la session
 
     <body>
         <header>
-            <div id='btnconnexion'>
+            <div>
                 <img src="img/title.png" max-width="100%">
             </div>
+        </header>
             <?php
-			//on utilise une variable page qui irra chercher sur la page aillant le même nom le contenu à afficher sur index.php
 		if (Authentification::getInstance()->isAuth()==false) // Test de la connexion de l'utilisateur, si non connecté :
 		{ ?>
-		      
-                <div id='btnconnexion'>
+                <div class='btn'>
                     <a href="index.php?page=connexion">
-                        <input type="button" name="Connexion " value="connexion" />
+                        <input type="button" name="Connexion " value="Connexion" />
                     </a>
                 </div>
                 <?php }
 		else { // Si utilisateur connecté :
         echo "Welcome ".$_SESSION['user_name']." !<br>"; ?>
-                    <div id='btnconnexion'>
+                    <div class='btn'>
                         <a href="index.php?page=upload">
                             <input type="button" name="upload " value="Upload" />
                         </a>
                     </div>
-                    <div id='btnconnexion'>
+                    <div class='btn'>
                         <a href="index.php?page=deconnexion">
                             <input type="button" name="déconnexion " value="Déconnexion" />
                         </a>
                         <?php } ?>
                     </div>
-                    <div id='btnconnexion'>
+                    <div class='btn'>
                         <a href="index.php?page=recherche">
                             <input type="button" name="recherche " value="Recherche" />
                         </a>
-                    </div>
-        </header>
-        <?php // Si la page est précisée, on la passe en paramètre, sinon accueil est par défaut
+                    </div> <?php // Si la page est précisée, on la passe en paramètre, sinon accueil est par défaut
         if(!empty($_GET['page']))
         {
 	        $page = $_GET['page'];
         ?>
+        <div class='btn'>
             <a href="index.php">
                 <input type="button" name="accueil " value="Accueil" />
-            </a>
+            </a></div>
             <?php
         } else
         {
@@ -62,7 +60,7 @@ Session::getInstance()->start(); // Démarrage de la session
         }
 		
 		include($page.'.php'); ?>
-
+       
                 <footer>
                     <img src="img/footer.png" max-width="100%">
                 </footer>
