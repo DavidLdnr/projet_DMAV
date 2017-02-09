7b474a229a49df25b025c19aa6eb9b6e9775fc28
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-02-09 12:32:09 +0100

    comit

--- FormUpload.class.php
+++ FormUpload.class.php
@@ -2,8 +2,7 @@
 class FormUpload {
 function __toString(){
 	       return ("<form action='index.php?page=upload' method='post' enctype='multipart/form-data'>
-                        <label name='titre'>Titre</label><input type='text' name='titre' value='' autofocus required><br>
-                        <label name='description'>Description</label><input type='text' name='description' value='' required>
+                        <label name='description'>Description</label><input type='text' name='description' value='' autofocus required>
                         <input type='file' name='file' required></br> Choisissez le type de fichier que vous voulez upload</br>
                         <input type='radio' name='type' value='1'>Video
                         <input type='radio' name='type' value='2'>Audio
