dab682822a7b310c514d7d43edff32cf887369d4
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-02-09 17:07:13 +0100

    commentaire du formconnexion

--- FormConnexion.class.php
+++ FormConnexion.class.php
@@ -1,4 +1,5 @@
 <?php 
+//création du formulaire de connexion via la méthode __toString() 
 class FormConnexion {
 function __toString(){
 	       return ("<form action='index.php?page=connexion' method='post'><label name='pseudo'>Pseudo</label><input type='text' name='pseudo' value='' autofocus required><br><label name='mdp'>Mot de passe</label><input type='password' name='mdp' value='' required><input type='submit' value='Valider'></form>");
